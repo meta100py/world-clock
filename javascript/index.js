c23544bb96ca3acf61e0428f3697473513d969be
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-05-23 14:03:14 -0300

    Added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -43,6 +43,7 @@ function updateCity(event) {
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
         </div>
+        <a href="index.html">All cities</a>
   `;
 }
 updateTime();
